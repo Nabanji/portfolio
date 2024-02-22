ee98bf474c6b1a9b37af6fee8a7638fd81b81c52
Prince Nabanji <pnabanji@gmail.com>
2024-02-22 23:42:17 +0300

    Navigation update

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Sidebar selector of the nav
+//const sideBar = document.querySelector(".side-bar");
 const sideBar = document.querySelector(".side-bar");
 
 // Button selector for the sidebar button
@@ -12,16 +13,41 @@ let numberOfClicks = 0;
 
 // Setting the functionality of the button
 sidebarBtn.addEventListener("click", () => {
-    numberOfClicks = numberOfClicks + 1;
+    numberOfClicks++;
 
-    if (numberOfClicks % 2 === 0 || numberOfClicks === 0) {
-        sideBar.style.transform = "translateY(-280px)";
-        homeContent.style.transform = "translateY(0px)";
-        homeContent.style.opacity = "1";
+    if (numberOfClicks > 0 && numberOfClicks % 2 === 1) {
+        sideBar.style.transition = ".6s all ease-in-out";
+        homeContent.style.display = "none";
+        sideBar.style.display = "block";
+        sideBar.style.transform = "translateY(0px)";
+        console.log(numberOfClicks);
     } else {
-        sidebarBtn.style.alignSelf = "flex-start";
-        sideBar.style.transform = "translateY(20px)";
-        homeContent.style.opacity = "0";
-        homeContent.style.transition = ".6s all ease-in-out";
+        homeContent.style.display = "flex";
+        homeContent.style.transition = "2s all ease-in-out";
+        sideBar.style.display = "none";
     }
 })
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Smooth scrolling for navigation links
+    const navLinks = document.querySelectorAll('.nav-link');
+    navLinks.forEach(link => {
+        link.addEventListener('click', smoothScroll);
+    });
+
+    function smoothScroll(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+        if (targetSection) {
+            const offsetTop = targetSection.offsetTop;
+            window.scrollTo({
+                top: offsetTop,
+                behavior: 'smooth'
+            });
+            sideBar.style.display = "none";
+            homeContent.style.display = "flex";
+        }
+    }
+});
